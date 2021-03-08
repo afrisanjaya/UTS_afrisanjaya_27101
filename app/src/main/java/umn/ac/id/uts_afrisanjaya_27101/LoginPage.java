a17006500611aab2a59af9122e2b515bc0625ae5
afrisanjaya <47937342+afrisanjaya@users.noreply.github.com>
2021-03-09 01:46:55 +0700

    fix xml
    1-3 selesai

--- app/src/main/java/umn/ac/id/uts_afrisanjaya_27101/LoginPage.java
+++ app/src/main/java/umn/ac/id/uts_afrisanjaya_27101/LoginPage.java
@@ -2,13 +2,33 @@ package umn.ac.id.uts_afrisanjaya_27101;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginPage extends AppCompatActivity {
+    EditText username, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        username = (EditText) this.findViewById(R.id.Username);
+        password = (EditText) this.findViewById(R.id.Password);
+        login = (Button) this.findViewById(R.id.button2);
+
+
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if((username.getText().toString().equals("uasmobile"))&&(password.getText().toString().equals("uasmobilegenap"))){
+                    Intent intentSatu = new Intent(LoginPage.this, ListLagu.class);
+                    startActivity(intentSatu);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/umn/ac/id/uts_afrisanjaya_27101/MainActivity.java
+++ app/src/main/java/umn/ac/id/uts_afrisanjaya_27101/MainActivity.java
@@ -2,13 +2,35 @@ package umn.ac.id.uts_afrisanjaya_27101;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnPageLogin, btnPageProfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPageLogin = findViewById(R.id.LoginPage);
+        btnPageProfil = findViewById(R.id.ProfilPage);
+
+        btnPageLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentSatu = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(intentSatu);
+            }
+        });
+        btnPageProfil.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentDua = new Intent(MainActivity.this, ProfilSaya.class);
+                startActivity(intentDua);
+            }
+        });
     }
 }
